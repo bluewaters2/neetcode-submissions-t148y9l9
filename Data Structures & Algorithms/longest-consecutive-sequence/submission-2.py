81eb20b7a3c4ef0538afb1285b4ed6d2bb2074d0
Sohini Ghosh <144217053+bluewaters2@users.noreply.github.com>
2026-05-14 17:55:06 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,17 @@
+# create a set to reduce lookup to O(1), we iterate through the array and then check whether the consecutive numbers are present in set
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        seen = set(nums)
+        ans = 0
+
+        for num in nums:
+            j, cnt = num, 0
+            while j in seen:
+                cnt += 1
+                j += 1
+
+            ans = max(ans, cnt)
+        
+        return ans
