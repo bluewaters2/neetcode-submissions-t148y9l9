61c8b4abbf460bb6747861b01f778f09e3d0d26a
Sohini Ghosh <144217053+bluewaters2@users.noreply.github.com>
2026-05-15 12:54:24 +0530

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,29 @@
+# we can use 3 for loops to solve this problem but TC - O(n^3)
+# reduce TC by sorting and then use left and right pointers to find the pairs - TC: O(nlogn)
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        l = len(nums)
+        nums.sort()
+        ans = set()
+        i = 0
+
+        while i < l:
+            left, right = i+1, l-1
+            while left < right:
+                if nums[left] + nums[right] + nums[i] == 0:
+                    ans.add((nums[left], nums[right], nums[i]))
+                    left += 1
+
+                elif nums[left] + nums[right] + nums[i] > 0:
+                    right -= 1
+                
+                else:
+                    left += 1
+                
+
+            i += 1
+            while i < len(nums) and nums[i-1] == nums[i]:
+                i += 1
+        
+        return list(ans)
